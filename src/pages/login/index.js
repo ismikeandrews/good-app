f66317a0257ccdaa59f19f203c3ac8813792056b
Cyntia Sayuri <cy.sayuri@hotmail.com>
2020-09-17 00:13:45 -0300

    Tela de Login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
 import styles from './styles'
 
 class Login extends Component{
@@ -10,26 +10,38 @@ class Login extends Component{
     
     render(){
         return(
-            <View style = { styles.container }>
-                <Text style = { styles.title }>Seja bem-vindo!</Text>
-                <Text style = { styles.subtitle }>Faça login para acessar a plataforma</Text>
+            <View style={ styles.container }>
+                <View style={ styles.logo }>
+                    <Image style={ styles.logoImg } source={require('../../assets/images/ijc.png')} />
+                </View>
+
+                <Text style={ styles.title }>Seja bem-vindo!</Text>
+                <Text style={ styles.subtitle }>Faça login para acessar a plataforma</Text>
             
-                <Text style = { styles.label }>Login</Text>
-                <TextInput style = { styles.input } placeholder = 'Digite seu login'
-                onChangeText = { text => onChangeText(text) }/> 
+                <Text style={ styles.label }>Login</Text>
+                <TextInput style={ styles.input } placeholder = 'Digite seu login'
+                onChangeText={ text => onChangeText(text) }/> 
                 
-                <Text style = { styles.label }>Senha</Text>
-                <TextInput style = { styles.input } placeholder = 'Digite sua senha'
-                onChangeText = {text => onChangeText(text)}/>
+                <Text style={ styles.label }>Senha</Text>
+                <TextInput style={ styles.input } placeholder='Digite sua senha'
+                onChangeText={text => onChangeText(text)}/>
                 
-                <Button style = { styles.btnEntrar } title = "Entrar"
-                onPress = {() => this.login()}/>
+                <TouchableOpacity style={[ styles.btn, styles.btnEntrar ]}
+                onPress={() => this.login()}>
+                    <Text style={[ styles.btnText, styles.btnTextEntrar ]}>Entrar</Text>
+                </TouchableOpacity>
                 
-                <Button style = { styles.btn } title = "Cadastro"
-                onPress = {() => this.props.navigation.navigate('Cadastro')}/>
-                
-                <Button style = { styles.btn } title = "Código de cadastro"
-                onPress = {() => this.props.navigation.navigate('CadastroCurriculo')}/>
+                <View style={ styles.contentBtn }>
+                    <TouchableOpacity style={[ styles.btn, styles.btns, styles.btnCadastro ]}
+                    onPress={() => this.props.navigation.navigate('Cadastro')}>
+                        <Text style={[ styles.btnText, styles.btnTextCadastro ]}>Cadastro</Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity style={[ styles.btn, styles.btns, styles.btnCodigo ]}
+                    onPress={() => this.props.navigation.navigate('CadastroCurriculo')}>
+                        <Text style={[ styles.btnText, styles.btnTextCodigo ]}>Código de cadastro</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
             )
         }
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -3,13 +3,23 @@ import Constants from 'expo-constants'
 
 import color from '../../assets/utils/colors'
 import font from '../../assets/utils/fonts'
+import { preventAutoHide } from 'expo/build/launch/SplashScreen';
 
 export default StyleSheet.create({
     container: {
         backgroundColor: color.white,
         flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
+        paddingHorizontal: 25,
+        paddingTop: Constants.statusBarHeight + 30,
+    },
+    logo: {
+        alignItems: 'center',
+        width: '100%',
+    },
+    logoImg: {
+        height: 72,
+        marginVertical: 40,
+        width: 100,
     },
     title: {
         color: color.gray,
@@ -29,22 +39,46 @@ export default StyleSheet.create({
     input: {
         backgroundColor: 'white',
         borderColor: color.gray,
-        borderRadius: 7,
+        borderRadius: 10,
         borderWidth: 1,
         color: color.gray,
         height: 45,
         paddingLeft: 15,
     },
-    btnEntrar: {
-        backgroundColor: color.pink,
-        color: 'white',
-        marginTop: 30,
+    contentBtn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
     },
     btn: {
-        backgroundColor: color.white,
-        borderColor: color.gray,
-        color: color.gray,
-        borderRadius: 15,
-        width: '100%',
-    }
+        borderRadius: 30,
+    },
+    btns: {
+        backgroundColor: 'transparent',
+        borderWidth: 1,
+        width: '48%',
+    },
+    btnText: {
+        paddingVertical: 10,
+        textAlign: 'center',
+    },
+    btnEntrar: {
+        backgroundColor: color.pink_light,
+        marginVertical: 30,
+    },
+    btnCadastro: {
+        borderColor: color.dark_red,
+    },
+    btnCodigo: {
+        borderColor: color.orange,
+    },
+    btnTextEntrar: {
+        color: color.white,
+    },
+    btnTextCadastro: {
+        color: color.dark_red,
+    },
+    btnTextCodigo: {
+        color: color.orange,
+    },
 })
